8f630159c16532fb26e08adcd2ede78fc0f2f8f8
Gilles Regamey <gillesoff@gmail.com>
2023-04-18 13:08:44 +0200

    Basic UI Window setup

--- GodotEnv/Scenes/Main/UI.gd
+++ GodotEnv/Scenes/Main/UI.gd
@@ -1,18 +1,16 @@
-extends TextureButton
+extends ReferenceRect
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var g_success = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Opening: startbutton ready.")
+	pass # Replace with function body.
 
-func _pressed():
-	print("start button pressed !")
-	g_success = get_tree().change_scene("res://Scenes/Main.tscn")
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
